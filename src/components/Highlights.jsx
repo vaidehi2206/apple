48f393abc27a655fef3ec79ee74a9eccafb28ae7
vaidehi2206 <vaidehivikramsinh.dabhi01@student.csulb.edu>
2024-09-12 16:16:38 -0700

    Updated index.html, App.jsx, and styles to implement new UI design; removed react.svg; added new components and utility classes in tailwind.config.js

--- src/components/Highlights.jsx
+++ src/components/Highlights.jsx
@@ -1,6 +1,28 @@
+import { useGSAP } from "@gsap/react"
+import gsap from "gsap"
+import { watchImg } from "../utils"
+
 const Highlights = () => {
+  useGSAP(() => {
+    gsap.to('#title', { opacity: 1, y:0 })
+    gsap.to('.link', {opacity:1, y:0 })
+  }, [])
   return (
-    <div>Highlights</div>
+    <section id='highlights' className="w-screen overflow-hidden h-full common-padding bg-zinc">
+        <div className="screen-max-width">
+          <div className="mb-12 w-full items-end justify-between">
+            <h1 id="title" className="section-heading">
+              Get the highlights.
+            </h1>
+            <div className="flex flex-wrap items-end gap-5">
+              <p className="link">
+                Watch the film
+                <img  src={watchImg} alt="watch" className="ml-2"/>
+              </p>
+            </div>
+          </div>
+        </div>
+    </section>
   )
 }
 
